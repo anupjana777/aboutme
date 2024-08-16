60f86e7a3034ac886aeaedc8228c7c9ff939879d
Anup Jana <113670678+anupjana777@users.noreply.github.com>
2024-08-16 23:00:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Teaching", "IQAC", "IEEE", "Accreditation Work", "Web development"],
+    strings: ["Academics(IIT)", "Teaching", "IQAC", "IEEE", "Accreditation Work"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
